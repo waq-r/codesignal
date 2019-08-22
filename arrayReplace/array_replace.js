0d63c278dcb8bd0cefd6c465da023371c61cdea1
waqar3 <waqar3@gmail.com>
2019-08-22 18:28:16 +0500

    git conflict resolution

--- arrayReplace/array_replace.js
+++ arrayReplace/array_replace.js
@@ -1,8 +1,4 @@
 function arrayReplace(inputArray, elemToReplace, substitutionElem) {
     return inputArray.map((ielem, i) => ielem === elemToReplace ? substitutionElem : ielem);
-<<<<<<< HEAD
 
 }
-=======
-}
->>>>>>> f6fa6e8dbf22acf8a938bc710b3327e6aabe3737
