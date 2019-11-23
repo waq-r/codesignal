bf4347868064c7f76fdebdddbccff1d04282b560
Vaqar <waqar3@gmail.com>
2019-11-23 20:02:31 +0500

    Update matrix_elements_sum.php

--- matrix_elements_sum.php
+++ matrix_elements_sum.php
@@ -52,23 +52,20 @@ The total price of all the rooms that are suitable for the CodeBots to live in.
 */
 
 function matrixElementsSum($matrix) {
-    //transpose array, making matrix columns rows
-    $transpose = array();
-    var_dump(sizeof($matrix));
-    for ($i = 0; $i <= sizeof($matrix[0]); $i++) {
-        $transpose[] = array_column($matrix, $i);
-    }
-    //count until theres no 0, break if u hit one
-    $count = 0;
-    foreach($transpose as $key => $value) {
-        foreach($value as $k => $v) {
-            if ($v == 0) {
-                break;
+    $index = [];
+    $sum = 0;
+    for($i = 0; $i<sizeof($matrix); $i++){
+        foreach($matrix[$i] as $k=>$v){
+            if(!in_array($k, $index)){
+                $sum += $v;
             }
-            $count = $count + $v;
+            if($v === 0){
+                $index[] = $k;
+
+            }
         }
     }
-    return $count;
+    return $sum;
 }
 
 
