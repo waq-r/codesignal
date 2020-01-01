38ac5a21c9ca6470c4b2ac5fb381bfa91c5ecdfc
Vaqar <waqar3@gmail.com>
2020-01-01 20:11:32 +0000

    Create deposit_profit.php

---
+++ depositProfit/deposit_profit.php
@@ -0,0 +1,8 @@
+function depositProfit($deposit, $rate, $threshold) {
+    $i = 0;
+    while($deposit < $threshold){
+        $i++;
+        $deposit += $deposit * $rate/100;
+    }
+    return $i;
+}
