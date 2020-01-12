25c09846b7c8ed3e8ba633384f44958531b4fec5
Vaqar <waqar3@gmail.com>
2020-01-12 19:50:47 +0000

    Create build_palindrome.php

---
+++ buildPalindrome/build_palindrome.php
@@ -0,0 +1,11 @@
+function buildPalindrome($st) {
+    $i = 0;
+    $oldSt = $st;
+    
+    while($st != strrev($st)){
+        $st = $oldSt . strrev(substr($oldSt,0, $i));
+        $i++;
+    }
+    
+    return $st;
+}
